2393887e168b6b372ed7cf015b02974f51b2f56a
ayush <ayushkhobragade17@gmail.com>
2024-01-15 14:34:37 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,41 @@
-  import express from "express";
-  import cors from "cors";
-  import { PORT, mongoDBURL } from "./config.js";
-  import mongoose from "mongoose";
-  import bookRoutes from "./routes/bookRoutes.js";
-  import authRoutes from "./routes/authRoutes.js";
+import express from "express";
+import cors from "cors";
+import 'dotenv/config'
+import mongoose from "mongoose";
+import bookRoutes from "./routes/bookRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
-  const app = express();
+const app = express();
 
-  // Middleware for parsing request body
-  app.use(express.json());
+// Middleware for parsing request body
+app.use(express.json());
 
-  // middleware for handling cros policy
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type"]
-    })
-  );
+// middleware for handling cros policy
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
-  app.get("/", (request, response) => {
-    console.log(request);
-    return response.status(234).send("Welcome to Book Store");
-  });
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to Book Store");
+});
 
-  app.use("/books", bookRoutes);
-  app.use("/auth", authRoutes);
+app.use("/books", bookRoutes);
+app.use("/auth", authRoutes);
 
-  mongoose
-    .connect(mongoDBURL)
-    .then(() => {
-      console.log("APP connected with DB");
-      app.listen(PORT, () => {
-        console.log(`listening to port: ${PORT}`);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("APP connected with DB");
+    app.listen(process.env.PORT, () => {
+      console.log(`listening to port: ${process.env.PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
