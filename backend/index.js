be8893fd7082a59b31941a79dc143ae4f06438d1
ayush <ayushkhobragade17@gmail.com>
2024-08-28 13:31:59 +0530

    changed the index file

--- backend/index.js
+++ backend/index.js
@@ -10,42 +10,43 @@ const app = express();
 // Middleware for parsing request body
 app.use(express.json());
 
-// middleware for handling cros policy
-app.use(
-  cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "https://mern-book-store-frontend.vercel.app"
-        : process.env.NODE_ENV === "development"
-        ? "http://localhost:5173"
-        : "http://localhost:3001", // Set to empty string or your desired default for other cases
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
-app.use(cors());
+// CORS setup based on NODE_ENV
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === "production"
+      ? "https://mern-book-store-frontend.vercel.app"
+      : process.env.NODE_ENV === "development"
+      ? "http://localhost:5173"
+      : "http://localhost:3001",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+};
 
-app.get("/", (request, response) => {
-  console.log(request);
-  return response.status(234).send("Welcome to Book Store");
+app.use(cors(corsOptions));
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the Book Store API");
 });
 
 app.use("/books", bookRoutes);
 app.use("/auth", authRoutes);
 
+// Database connection and server start
 mongoose
-  .connect(process.env.NODE_ENV === "production"
-  ? process.env.MONGODB_URI : "mongodb://127.0.0.1:27017")
+  .connect(
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGODB_URI
+      : "mongodb://127.0.0.1:27017"
+  )
   .then(() => {
-    console.log("APP connected with DB");
+    console.log("Connected to MongoDB");
     app.listen(process.env.PORT, () => {
-      console.log(`listening to port: ${process.env.PORT}`);
+      console.log(`Server listening on port ${process.env.PORT}`);
     });
   })
   .catch((error) => {
-    console.log("could not connect to DB");
-    console.log("MONGODB_URI", process.env.MONGODB_URI);
-    console.log("LOCAL_MONGODB_URI", process.env.LOCAL_MONGODB_URI);
-    console.log(error);
+    console.error("Error connecting to MongoDB:", error.message);
   });
+
 export default app;
